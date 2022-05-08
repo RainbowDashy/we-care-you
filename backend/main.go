de9d4f814c357c978873acb6cdfc121b944faf3c
Kelo <meetkelo@outlook.com>
2022-05-08 13:55:42 +0800

    insert and get user

---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/RainbowDashy/we-care-you/backend/store"
+	"github.com/gin-gonic/gin"
+)
+
+var db *sql.DB
+
+func init() {
+	db, _ = store.OpenDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ backend/user.go
@@ -0,0 +1,22 @@
+package main
+
+type User struct {
+	Id       int
+	Username string
+	Password string
+}
+
+func InsertUser(user *User) error {
+	_, err := db.Exec("INSERT INTO user(username, password_hash VALUES(?, ?)", user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetUser(id int) (*User, error) {
+	row := db.QueryRow("SELECT * FROM user WHERE id = ?", id)
+	user := &User{}
+	err := row.Scan(&user.Id, &user.Username, &user.Password)
+	return user, err
+}
