773be571819274edb3cd17a34245e6cacc15f3f1
p0ny <p0uy@outlook.com>
2022-05-15 17:06:25 +0800

    reinit frontend, use router & pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-createApp(App).mount('#app')
+import App from "./App.vue"
+import router from "./router"
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount("#app")
