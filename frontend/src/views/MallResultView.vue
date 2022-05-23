f88ab0f0154bb1b5e31fc63d7919b404a7a3b7d4
p0ny <p0uy@outlook.com>
2022-05-23 21:48:46 +0800

    jump between mall view & mall res view

--- frontend/src/views/MallResultView.vue
+++ frontend/src/views/MallResultView.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { useRoute } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 import MallResultTable from "../components/MallResultTable.vue"
 import ExcelExportComponent from "../components/ExcelExportComponent.vue"
-
+import { NButton } from "naive-ui"
+const router = useRouter()
 const route = useRoute()
 const mallId = parseInt(route.params.id)
+const returnToPurchase = () => {
+  router.push(`/malls/${mallId}`)
+}
 </script>
 
 <template>
+  <n-button @click="returnToPurchase">返回购买</n-button>
   <MallResultTable :mallId="mallId" />
   <ExcelExportComponent :mallId="mallId" />
 </template>
--- frontend/src/views/MallView.vue
+++ frontend/src/views/MallView.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { useRoute } from "vue-router"
+import { NButton } from "naive-ui"
+import { useRoute, useRouter } from "vue-router"
 import ItemList from "../components/ItemList.vue"
 
 const route = useRoute()
+const router = useRouter()
 const mallId = parseInt(route.params.id)
+
+const jumpToView = () => {
+  router.push(`/malls/view/${mallId}`)
+}
 </script>
 
 <template>
+  <n-button @click="jumpToView">查看购买记录</n-button>
   <ItemList :mallId="mallId" />
 </template>
