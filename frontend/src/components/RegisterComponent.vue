278384a2cf6cf5b239079357552fffa6ac7309d7
John_Wilson <727706242@qq.com>
2022-05-24 21:37:01 +0800

    Merge branch 'main' of github.com:RainbowDashy/we-care-you

--- frontend/src/components/RegisterComponent.vue
+++ frontend/src/components/RegisterComponent.vue
@@ -4,20 +4,25 @@ import { useRouter } from "vue-router"
 import http from "../http.js"
 import { useUserStore } from "../stores/user.js"
 import { NButton, NForm, NFormItem, NInput } from "naive-ui"
+import { useMessage } from "naive-ui";
 const router = useRouter()
 const user = useUserStore()
+
 const input = reactive({
   username: "",
   password: "",
   location: "",
 })
+const message=useMessage()
 const registerAndLogin = async () => {
   try {
     await http.post("/users", {
       body: JSON.stringify(input),
     })
+    message.success("注册成功")
   } catch (err) {
     console.log(err)
+    message.error("注册失败")
     return
   }
 
@@ -32,9 +37,9 @@ const registerAndLogin = async () => {
 }
 </script>
 <template>
-  <div>
+  <div >
     <h1>注册</h1>
-    <p>创建新的账户</p>
+    <h2>创建新的账户</h2>
     <n-form>
       <n-form-item>
         <n-input v-model:value="input.username" placeholder="用户名" />
@@ -49,6 +54,7 @@ const registerAndLogin = async () => {
       <n-form-item>
         <n-input v-model:value="input.location" placeholder="地址" />
       </n-form-item>
+      <div style="display:flex;justify-content: center">
       <n-form-item>
         <n-button
           attr-type="button"
@@ -59,6 +65,7 @@ const registerAndLogin = async () => {
           注册
         </n-button>
       </n-form-item>
+      </div>
     </n-form>
   </div>
 </template>
@@ -66,11 +73,11 @@ const registerAndLogin = async () => {
 <style scoped>
 h1 {
   color: #227700;
+  text-align: center;
 }
-p {
+h2 {
   color: #8b8b8b;
-  font-size: 10px;
-  text-indent: 2em;
-  line-height: 15px;
+  text-align: center;
 }
+ 
 </style>
